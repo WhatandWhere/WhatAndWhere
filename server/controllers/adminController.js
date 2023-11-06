30be20ad38382ad33ad1e6172f1ec35479e3a5ea
AlexKorolewa42 <timtau2004@gmail.com>
2023-11-06 16:20:11 +0100

    Added logic to adminController

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,19 +1,57 @@
 const ApiError = require('../error/ApiError');
-const { User } = require('../models/models');
+const { User, UserInfo } = require('../models/models');
 
 class AdminController {
     async listUsers(req, res, next) {
-        // List all users logic
+        try {
+            const users = await UserInfo.findAll();
+            return res.status(200).json(users);
+
+        } catch (error) {
+            console.error('Sequelize error:', error);
+            next(ApiError.internal(`An error occurred while retrieving filtered events: ${error.message}`));
+        }
+
     }
 
     async updateUser(req, res, next) {
-        // Update user roles or permissions logic
-        // ...
+        try {
+            const { userId } = req.params;
+            const updatedData = req.body;
+
+            const [updated] = await UserInfo.update(updatedData, {
+                where: { userInfoId: userId }
+            });
+
+            if (updated) {
+                const updatedUser = await UserInfo.findByPk(userId);
+                return res.status(200).json(updatedUser);
+            }
+
+            throw new Error('User not found');
+
+        } catch (error) {
+            next(ApiError.internal('User not found'));
+        }
     }
 
     async deleteUser(req, res, next) {
-        // Delete user logic
-        // ...
+        try {
+            const { userId } = req.params;
+            const deleted = await UserInfo.destroy({
+                where: { userInfoId: userId }
+            });
+
+            if (deleted) {
+                return res.status(200).json({
+                    message: "User deleted successfully"
+                });
+            }
+
+            throw new Error('User not found');
+        } catch (error) {
+            next(ApiError.internal('User not found'));
+        }
     }
 }
 
