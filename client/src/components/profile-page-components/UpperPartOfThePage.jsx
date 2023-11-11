8e1bf6d40a13fce2aab9873d95d72dec20b717ec
MPokotilov <mpokotilov2@gmail.com>
2023-11-11 16:22:45 +0100

    Added the design and functionality to the profile pageto the
    Added a design to the Profile page. and added functionality to the button(When it is pressed you are able to change the settings of you profile. F.e: You can change the profile image, phone and email.)

--- client/src/components/profile-page-components/UpperPartOfThePage.jsx
+++ client/src/components/profile-page-components/UpperPartOfThePage.jsx
@@ -1,17 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../design-files-css/profile-page-css/UpperPartOfThePage.css';
 
 function UpperPartOfThePage() {
+    const username = "MockUsername";
+    const userEmail = "m1@gmail.com";
+    const userPhone = "+9900000000";
+    const userProfilePic = "/placeholder-avatar.png";
+    const [isEditing, setIsEditing] = useState(false);
+    const [isButtonBold, setIsButtonBold] = useState(false);
+    const [email, setEmail] = useState(userEmail);
+    const [phone, setPhone] = useState(userPhone);
+    const [profilePic, setProfilePic] = useState(userProfilePic);
+
+    const handleButtonClick = () => {
+        setIsEditing(!isEditing); // Toggle the editing mode
+        if (isEditing) {
+            // If we're not in edit mode, we will save the current values
+            console.log('Values saved:', { email, phone });
+            alert('Values saved!');
+        }
+        setIsButtonBold(!isButtonBold);
+    };
+
+    // Function to handle profile picture change
+    const handleProfilePicChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            // Assuming you want to display the image without saving it to a server
+            setProfilePic(URL.createObjectURL(file));
+        }
+    };
+
     return (
         <div className="user-profile">
-            <div className="user-image">
-                <img src="/placeholder-avatar.png" alt="User Placeholder" className="profile-pic" />
-            </div>
+            {isEditing ? (
+                <div className="user-image">
+                    <input
+                        type="file"
+                        className="profile-pic-upload"
+                        onChange={handleProfilePicChange}
+                        accept="image/*"
+                    />
+                </div>
+            ) : (
+                <div className="user-image">
+                    <img src={profilePic} alt="User" className="profile-pic" />
+                </div>
+            )}
             <div className="user-details">
-                <input placeholder="Name" className="input-field" />
-                <input placeholder="Surname" className="input-field" />
-                <input placeholder="Username" className="input-field" />
-                <button className="edit-button">Edit user details</button>
+                <div className="text-display">{username}</div>
+                {isEditing ? (
+                    <>
+                        <input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email"
+                            className="input-field"
+                        />
+                        <input
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            placeholder="Phone"
+                            className="input-field"
+                        />
+                    </>
+                ) : (
+                    <>
+                        <div className="text-display">{email}</div>
+                        <div className="text-display">{phone}</div>
+                    </>
+                )}
+                <button onClick={handleButtonClick}
+                        className={`edit-button ${isButtonBold ? 'edit-button-bold' : ''}`}>
+                    {isEditing ? 'Save changes' : 'Edit user details'}
+                </button>
+
             </div>
         </div>
     );
